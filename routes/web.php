8757d040a99e2bd1308f19edeba4bdedb8a7d02d
mamecurry <roddyroddyroddyroddy@gmail.com>
2024-09-09 17:57:30 +0900

    あいさつアプリ課題完了

--- routes/web.php
+++ routes/web.php
@@ -16,31 +16,47 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/comments/morning', function () {
+    return '<h1>朝のあいさつ</h1>
+    <h2>おはようございます</h2>';
+});
+
+Route::get('/comments/evening', function () {
+    return '<h1>夜のあいさつ</h1>
+    <h2>こんばんは</h2>';
+});
+
+Route::get('/comments/afternoon', function () {
+    return '<h1>昼のあいさつ</h1>
+    <h2>こんにちは</h2>';
+    });
+
 Route::get('/comments/night', function () {
     return '<h1>夜のあいさつ</h1>
     <h2>おやすみ</h2>';
-});
+    });
 
 Route::get('/comments/freeword/{msg}', function($msg) {
     return view('message.word', ['msg' => $msg]);
 });
 
-Route::get('/comments/random/{msg}', function($msg) {
-    return view('message.word', ['msg' => $msg]);
-});
-
 Route::get('/comments/random', function () {
     // ランダムなメッセージの配列
-    $greetings = [
+    $array = [
+        '<h1>ランダムなメッセージ</h1><h2>おはよう</h2>',
+        '<h1>ランダムなメッセージ</h1><h2>こんにちは</h2>',
+        '<h1>ランダムなメッセージ</h1><h2>こんばんは</h2>',
         '<h1>ランダムなメッセージ</h1><h2>おやすみ</h2>',
-        '<h1>ランダムなメッセージ</h1><h2>良い夢を</h2>',
-        '<h1>ランダムなメッセージ</h1><h2>おやすみなさい</h2>',
-        '<h1>ランダムなメッセージ</h1><h2>また明日</h2>',
     ];
 
-    // 配列からランダムに1つ選ぶ
-    $randomGreeting = $greetings[array_rand($greetings)];
+    // array_rand()でランダムに1つのキーを選択
+    $randomKey = array_rand($array);
 
-    // ランダムな挨拶を返す
-    return $randomGreeting;
+    // ランダムに選ばれたメッセージ
+    $randomMessage = $array[$randomKey];
+
+    return $randomMessage;
 });
+
+
